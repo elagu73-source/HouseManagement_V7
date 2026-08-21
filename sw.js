4fff59bc55cc4339f47574bd5bf0f4f032687a18
elagu73-source <elagu73@gmail.com>
2026-08-20 21:51:59 -0300

    sgf

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = "house-management-v1";
+
+const ARCHIVOS = [
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./icon-192.png",
+    "./icon-512.png",
+    "./apple-touch-icon.png"
+];
+
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(ARCHIVOS))
+    );
+
+    self.skipWaiting();
+
+});
+
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            )
+        )
+    );
+
+    self.clients.claim();
+
+});
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+        fetch(event.request)
+            .catch(() => caches.match(event.request))
+    );
+
+});
